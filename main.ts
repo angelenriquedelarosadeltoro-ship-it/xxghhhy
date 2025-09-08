c6531261585f51c55f721fd0f0a9f5cf7c46daf9
angelenriquedelarosadeltoro-ship-it <angelenriquedelarosadeltoro@gmail.com>
2025-09-07 21:54:40 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.showString("Hello!")
+music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
+led.plotBrightness(0, 0, 255)
+led.plotBarGraph(
+0,
+0
+)
+basic.forever(function () {
+	
+})
